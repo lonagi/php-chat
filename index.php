8174a2253277e427673eaf46fef87d414b5d2861
Sasha <lonagi22@gmail.com>
2020-11-28 20:56:04 +0200

    css

--- index.php
+++ index.php
@@ -5,7 +5,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <script src="https://ex.nvg-group.com/libs/jquery/3.4.1/f.min.js"></script>
-        <script src="client.js"></script>
+        <style>
+            #chat-response {
+                display: inline-block;
+                background: aliceblue;
+                padding: 20px;
+                font-size: 18px;
+                color: black;
+                border: 1px solid #9c9c9c;
+                width: 500px;
+                min-height: 200px;
+                overflow-y: scroll;
+                -ms-overflow-y: scroll;
+            }
+        </style>
     </head>
 <body>
 
