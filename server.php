e6a3e3c27514c1689faa32f517af9cce0a34fe71
Sasha <lonagi22@gmail.com>
2020-11-29 00:22:35 +0200

    some fix

--- server.php
+++ server.php
@@ -1,17 +1,17 @@
 <?php
 
-define("PORT","8090");
+define("PORT",8090);
 require_once "chat.php";
 
 $chat = new Chat();
 $socket = socket_create(AF_INET,SOCK_STREAM, SOL_TCP);
 socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($socket,"localhost",PORT);
+socket_bind($socket,0,PORT);
 socket_listen($socket);
 
 while(true) {
     $_socket = socket_accept($socket);
-    $header = socket_read($_socket,1024);
+    $header = socket_read($_socket,40960);
     $chat->send_headers($header, $_socket,"localhost",PORT);
 }
 
