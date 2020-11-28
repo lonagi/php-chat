73ee1ccfd2b6366a881eb0741d9372367ea98d06
Sasha <lonagi22@gmail.com>
2020-11-28 20:59:11 +0200

    some events about sockets

--- client.js
+++ client.js
@@ -2,3 +2,14 @@ function show_message(text) {
     $("#chat-response").append("<div>"+text+"</div>");
 }
     var socket = new WebSocket("ws://localhost:8090/server.php");
+    socket.onopen = function () {
+        show_message("The connection is established");
+    };
+
+    socket.onerror = function (e) {
+        show_message("ERROR " + (e.message ? e.message : ""));
+    };
+
+    socket.onclose = function () {
+        show_message("Disconnect");
+    };
