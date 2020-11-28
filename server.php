7cd8ffd3bd83ba047c7e9c1b4354e89432faccd8
Sasha <lonagi22@gmail.com>
2020-11-29 00:15:21 +0200

    Update server.php

---
+++ server.php
@@ -0,0 +1,20 @@
+<?php
+
+define("PORT","8090");
+require_once "chat.php";
+
+$chat = new Chat();
+$socket = socket_create(AF_INET,SOCK_STREAM, SOL_TCP);
+socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+socket_bind($socket,"localhost",PORT);
+socket_listen($socket);
+
+while(true) {
+    $_socket = socket_accept($socket);
+    $header = socket_read($_socket,1024);
+    $chat->send_headers($header, $_socket,"localhost",PORT);
+}
+
+socket_close();
+
+?>
