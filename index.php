adfa2f55a57aecf39aa765e1dfd86ef6bd0d7f7a
Sasha <lonagi22@gmail.com>
2020-11-28 20:45:28 +0200

    init project

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Chat</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <script src="https://ex.nvg-group.com/libs/jquery/3.4.1/f.min.js"></script>
+        <script src="client.js"></script>
+    </head>
+<body>
+
+</body>
+</html>
