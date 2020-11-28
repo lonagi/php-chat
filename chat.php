e794024ad218380a11fb750eb664b2b8312c5fcd
Sasha <lonagi22@gmail.com>
2020-11-29 00:15:19 +0200

    Create chat.php

---
+++ chat.php
@@ -0,0 +1,24 @@
+<?php
+
+class Chat {
+    public function send_headers($headers_query, $_socket, $host, $port) {
+        $headers = [];
+        $tmp = preg_split("/\r\n/",$headers_query);
+
+        foreach ($tmp as $t) {
+            $t = rtrim($t);
+            echo $t."\n\n\n";
+            if(preg_match('/\A(\S+): (.*)\z/', $t, $matches))
+                $headers[$matches[1]] = $matches[2];
+        }
+
+        $key = $headers['Sec-WebSocket-Key'];
+        $_key = base64_encode(pack("H*",sha1($key."258EAFA5-E914-47DA-95CA-C5AB0DC85B11")));
+
+        $send_headers = "HTTP/1.1 101 Switching Protocols \r\nUpgrade: websocket\r\nConnection: Upgrade\r\nWebSocket-Origin: $host\r\nWebSocket-Location: ws://$host:$port/server.php\r\nSec-WebSocket-Accept:$_key\r\n\r\n";
+
+        socket_write($_socket, $send_headers, strlen($send_headers));
+    }
+}
+
+?>
