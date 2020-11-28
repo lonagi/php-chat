ed7b2d7a16ab6c4e476f27c713b0b6024a121419
Sasha <lonagi22@gmail.com>
2020-11-28 21:02:41 +0200

    headers

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+header("X-Frame-Options: SAMEORIGIN");
+header("X-Content-Type-Options: nosniff");
+header("X-XSS-Protection: 1; mode=block");
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
